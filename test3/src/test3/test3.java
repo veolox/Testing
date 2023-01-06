a865c85e8d0f7648f6d41a2723f9ef67677b1c17
Veolox <Marco Li Gammari@192.168.1.184>
2023-01-06 17:24:53 +0100

    Second commit
    Signed-off-by: Veolox <Marco Li Gammari@192.168.1.184>

--- test3/src/test3/test3.java
+++ test3/src/test3/test3.java
@@ -5,10 +5,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class test3 extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -30,12 +35,35 @@ public class test3 extends JFrame {
 	 * Create the frame.
 	 */
 	public test3() {
+		setTitle("test");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton sendLogin = new JButton("New button");
+		sendLogin.setBounds(288, 204, 89, 23);
+		contentPane.add(sendLogin);
+		
+		textField = new JTextField();
+		textField.setBounds(24, 28, 205, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(24, 112, 205, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel matricolaVariableLogin = new JLabel("Matricola");
+		matricolaVariableLogin.setBounds(24, 11, 46, 14);
+		contentPane.add(matricolaVariableLogin);
+		
+		JLabel passwordVariableLogin = new JLabel("Password");
+		passwordVariableLogin.setBounds(24, 92, 46, 14);
+		contentPane.add(passwordVariableLogin);
 	}
-
 }
